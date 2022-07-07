adf4f8069c4efbd26088c874aea947bc30c770df
Rishi <rishiraj.mec18@itbhu.ac.in>
2022-07-07 19:24:10 +0530

    "Initial Commit"

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler,Filters
 import os
+from string import punctuation
+import telegram.ext
+import requests
 TOKEN=os.environ.get("TELEGRAM_ID")
 def start(update,context):
     yourname=update.message.chat.first_name
-    msg="Hi"+yourname+"! Welcome to Lingua Franca Vocab Bot"
+    msg="Hello "+yourname+"! Welcome to Lingua Franca Dictionary Bot"
     context.bot.send_message(update.message.chat.id,msg)
+
+def help(update, context):
+    update.message.reply_text("""
+    The following commands are available for you:
+
+    /start -> Welcome Message
+    /help -> This Message
+    /find /search /get -> Used to find the meaning, examples, synonyms and antonyms of any word.
+    /syno -> Used to search the synonyms of any word.
+    /anto -> Used to search the antonyms of any word.
+    /explain -> Used to search all data about a word.
+    /contact -> For suggestions and bug reports 
+    """)
 def details(update,context):
     context.bot.send_message(update.message.chat.id,str(update))
 
@@ -21,6 +37,7 @@ def main():
 
     ###################   Handlers :
     dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("details", details))
 
     dp.add_handler(MessageHandler(Filters.text,mimic))
