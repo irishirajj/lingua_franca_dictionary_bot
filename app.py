fa7c999ea38f242ed824794992050b11384f78b3
Rishi <rishiraj.mec18@itbhu.ac.in>
2022-07-07 21:23:05 +0530

    Updated the logic of creating the final antonym list

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from telegram.ext import Updater
-from telegram.ext import CommandHandler, MessageHandler,Filters
+from telegram.ext import CommandHandler, MessageHandler, Filters
 import os
 import json
 import requests
@@ -7,11 +7,14 @@ import requests
 from string import punctuation
 import telegram.ext
 
-TOKEN=os.environ.get("TELEGRAM_ID")
-def start(update,context):
-    yourname=update.message.chat.first_name
-    msg="Hello "+yourname+"! Welcome to Lingua Franca Dictionary Bot"
-    context.bot.send_message(update.message.chat.id,msg)
+TOKEN = os.environ.get("TELEGRAM_ID")
+
+
+def start(update, context):
+    yourname = update.message.chat.first_name
+    msg = "Hello " + yourname + "! Welcome to Lingua Franca Dictionary Bot"
+    context.bot.send_message(update.message.chat.id, msg)
+
 
 def help(update, context):
     update.message.reply_text("""
@@ -25,79 +28,85 @@ def help(update, context):
     /explain -> Used to search all data about a word.
     /contact -> For suggestions and bug reports 
     """)
-def anto(update,context):
+
+
+def anto(update, context):
     msg = f"{update.message.text}"
     word = msg[6:]
     antonyms = antoList(word)
-    if( len(antonyms) == 0):
+    if (len(antonyms) == 0):
         update.message.reply_text("Sorry! The word was not found in our dictionary.")
         return ""
-    word = "<b>"+word[0].upper()+word[1:]+"</b>"
+    word = "<b>" + word[0].upper() + word[1:] + "</b>"
     strng = u"\U0001F1EE\U0001F1F3" + " " + word + "\n\n" + u"\U0001F4DA <b>Antonyms</b> :\n" + antonyms
     update.message.reply_text(strng, parse_mode=telegram.ParseMode.HTML)
 
+
 def antoList(word):
     url = f"https://dictionaryapi.com/api/v3/references/thesaurus/json/{word}?key=06a00f95-9843-4f0f-9378-dec4f507c81b"
-    jeasons= requests.get(url).json()
-    if(len(jeasons)==0):
+    jeasons = requests.get(url).json()
+    if (len(jeasons) == 0):
         return
-    if(type(jeasons[0])==str):
+    if (type(jeasons[0]) == str):
         return
-    antonyms=set()
-    lenjeasons=len(jeasons)
+    antonyms = set()
+    lenjeasons = len(jeasons)
     for i in range(lenjeasons):
-        jeason_i=jeasons[i]
+        jeason_i = jeasons[i]
         if (jeason_i.__contains__('meta')):
-            meta_i=jeason_i['meta']
+            meta_i = jeason_i['meta']
             if (meta_i.__contains__('ants')):
-                ants_i=meta_i['ants']
-                len_ants_i=len(ants_i)
+                ants_i = meta_i['ants']
+                len_ants_i = len(ants_i)
                 for j in range(len_ants_i):
-                    ants_i_j=ants_i[j]
-                    len_ants_i_j=len(ants_i_j)
+                    ants_i_j = ants_i[j]
+                    len_ants_i_j = len(ants_i_j)
                     for k in range(len_ants_i_j):
                         antonyms.add(ants_i_j[k])
-    final_antonym_list=""
-    n=len(antonyms)
+    final_antonym_list = ""
+    n = len(antonyms)
     for i in range(n):
-        final_antonym_list += antonyms[i+1]
-        if(i!=n-1):
+        final_antonym_list += antonyms[i + 1]
+        if i != n - 1:
             final_antonym_list += ", "
     return final_antonym_list
 
 
+def details(update, context):
+    context.bot.send_message(update.message.chat.id, str(update))
 
-def details(update,context):
-    context.bot.send_message(update.message.chat.id,str(update))
 
-def mimic(update,context):
-    context.bot.send_message(update.message.chat.id,update.message.text)
+def mimic(update, context):
+    context.bot.send_message(update.message.chat.id, update.message.text)
 
-def error(update,context):
+
+def error(update, context):
     context.bot.send_message(update.message.chat.it, "OOps! Error encountered")
 
+
 def main():
-    updater=Updater(token=TOKEN)   #updater
-    dp=updater.dispatcher          #dispatcher
+    updater = Updater(token=TOKEN)  # updater
+    dp = updater.dispatcher  # dispatcher
 
     ###################   Handlers :
-    dp.add_handler(CommandHandler("start",start))
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("anto",anto))
+    dp.add_handler(CommandHandler("anto", anto))
     dp.add_handler(CommandHandler("details", details))
 
-    dp.add_handler(MessageHandler(Filters.text,mimic))
+    dp.add_handler(MessageHandler(Filters.text, mimic))
 
     dp.add_error_handler(error)
 
-    #updater.start_polling()
+    # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
-                          port=os.environ.get("PORT",443),
+                          port=os.environ.get("PORT", 443),
                           url_path=TOKEN,
-                          webhook_url="https://lingua-franca-dictionary-bot.herokuapp.com/"+TOKEN
+                          webhook_url="https://lingua-franca-dictionary-bot.herokuapp.com/" + TOKEN
                           )
 
     updater.idle()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
